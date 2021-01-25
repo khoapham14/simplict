fc51c034f3172e72f2ed97c66f3941868c556088
Cody <cyvn.fantasy@gmail.com>
2021-01-25 14:27:27 +1300

    Fixing average logic to accomodate new solves. Adding avg of 12 and avg of 50.

--- src/Components/statistics.js
+++ src/Components/statistics.js
@@ -7,35 +7,34 @@ class Stats extends React.Component {
   constructor() {
     super();
     this.state = {
-      record: [ 0 ],
-      avg5: 0,
-      avg12: 0,
-      avg50: 0,
+      record: [0],
+      ao5: 0,
+      ao12: 0,
+      ao50: 0,
       best: 0,
       worst: 0,
     };
 
-    this.handleSpace = this.handleSpace.bind(this)
     this.avg_of_5 = this.avg_of_5.bind(this)
+    this.avg_of_12 = this.avg_of_12.bind(this)
+    this.avg_of_50 = this.avg_of_50.bind(this)
     this.stringToInt = this.stringToInt.bind(this)
     this.clearRecord = this.clearRecord.bind(this)
-
   }
 
   componentDidMount() {
     window.addEventListener("resize", this.handleWindowSizeChange);
     window.addEventListener("spacebar", this.handleSpace, true);
+    setInterval(() => this.setState({
+      ao5: this.avg_of_5(),
+      ao12: this.avg_of_12(),
+      ao50: this.avg_of_50()
+    }), 1000)
   }
 
   componentWillUnmount() {
     window.removeEventListener("resize", this.handleWindowSizeChange);
     window.removeEventListener("spacebar", this.handleSpace, true);
-  }
-
-  handleSpace(e) {
-    if (e.keyCode === 32) {
-      this.setState({ record: Timer.exportTime() });
-    }
   }
 
   stringToInt(array) {
@@ -46,32 +45,57 @@ class Stats extends React.Component {
     if (this.props.record.length >= 5) {
       var i = this.props.record.length - 1;
       var solves = []
-      for (var x = 0; x <= 4; x++) {
+      for (var x = i; (i - x) < 5; x--) {
         solves = solves.concat(this.props.record[x])
       }
-
       this.setState({ record: solves })
-    }
-    else {
-      alert("Less than 5 solves");
+      this.state.record.sort(function (a, b) { return a - b });
+      this.setState({
+        best: this.state.record.shift(),
+        worst: this.state.record.pop(),
+        ao5: (this.stringToInt(this.state.record).reduce((a, b) => a + b, 0)) / 3,
+      });
     }
 
-    this.state.record.sort(function (a, b) { return a - b });
-    this.setState({
-      best: this.state.record.shift(),
-      worst: this.state.record.pop(),
-      ao5: (this.stringToInt(this.state.record).reduce((a, b) => a + b, 0))/3,
-    });
-
-    alert(this.state.record);
+    return this.state.ao5;
   }
 
   avg_of_12() {
+    if (this.props.record.length >= 12) {
+      var i = this.props.record.length - 1;
+      var solves = []
+      for (var x = i; (i - x) < 12; x--) {
+        solves = solves.concat(this.props.record[x])
+      }
+      this.setState({ record: solves })
+      this.state.record.sort(function (a, b) { return a - b });
+      this.setState({
+        best: this.state.record.shift(),
+        worst: this.state.record.pop(),
+        ao12: (this.stringToInt(this.state.record).reduce((a, b) => a + b, 0)) / 10,
+      });
+    }
 
+    return this.state.ao12;
   }
 
   avg_of_50() {
+    if (this.props.record.length >= 50) {
+      var i = this.props.record.length - 1;
+      var solves = []
+      for (var x = i; (i - x) < 50; x--) {
+        solves = solves.concat(this.props.record[x])
+      }
+      this.setState({ record: solves })
+      this.state.record.sort(function (a, b) { return a - b });
+      this.setState({
+        best: this.state.record.shift(),
+        worst: this.state.record.pop(),
+        ao12: (this.stringToInt(this.state.record).reduce((a, b) => a + b, 0)) / 48,
+      });
+    }
 
+    return this.state.ao50;
   }
 
   clearRecord() {
@@ -86,11 +110,12 @@ class Stats extends React.Component {
   render() {
     return (
       <div id="avg-container">
+        <Button onClick={this.clearRecord}> Clear Record </Button>
         <p id="avg-text"> {this.props.record} </p>
-        <p id="avg-text" onClick={this.avg_of_5}> ao5: {this.state.ao5} </p>
-        <p id="avg-text"> ao5: </p>
-        <p id="avg-text"> ao5: </p>
-        <Button onClick={this.clearRecord}> Clear </Button>
+        <p id="avg-text"> ao5: {this.state.ao5} </p>
+        <p id="avg-text"> ao12: {this.state.ao12}</p>
+        <p id="avg-text"> ao50: {this.state.ao50} </p>
+        
       </div>
     );
   }
