1fbf02f788b4a00fc165830a091269d7f8612d82
Cody <cyvn.fantasy@gmail.com>
2021-02-07 16:10:23 +1300

    Minor changes to the page icon & title

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,22 @@
+import renderer from 'react-test-renderer';
 
+import Timer from './Components/timer.js';
+import Stats from './Components/statistics.js';
+import Scrambler from './Components/scrambler.js';
+
+it('Timer renders correctly', () => {
+    const tree = renderer.create(<Timer />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+
+it('Stats renders correctly', () => {
+    const tree = renderer.create(<Stats />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+
+it('Scrambler renders correctly', () => {
+    const tree = renderer.create(<Scrambler />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
